73fe59ccc95948d613cac88738b0d71e80a62275
tribbianiwang <1130455004@qq.com>
2020-10-30 16:09:30 +0800

    CheckBoxDemo.dart

--- lib/CheckBoxDemo.dart
+++ lib/CheckBoxDemo.dart
@@ -5,13 +5,52 @@ class CheckBoxDemo extends StatefulWidget {
 }
 
 class _CheckBoxDemoState extends State<CheckBoxDemo> {
+  var flag = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("checkbox"),
       ),
-      body: Text("checkbox"),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+           Row(children: <Widget>[
+
+             Checkbox(
+               value: this.flag,
+               activeColor: Colors.red,
+               onChanged: (value){
+                 setState(() {
+                   this.flag = value;
+                 });
+
+             },),
+
+           ],),
+          Row(
+            children: <Widget>[
+              Text(this.flag?"选中":"未选中"),
+            ],
+          ),
+          SizedBox(height: 40,),
+
+          CheckboxListTile(
+            value: this.flag,
+            onChanged: (value){
+              setState(() {
+                this.flag = value;
+              });
+            },
+            title: Text("标题"),
+            subtitle: Text("这是一个二级标题"),
+            secondary: Icon(Icons.radio),
+
+          )
+
+        ],
+      ),
     );
   }
 }
--- lib/FieldDemo.dart
+++ lib/FieldDemo.dart
@@ -5,65 +5,119 @@ class FieldDemo extends StatefulWidget {
 }
 
 class _FieldDemoState extends State<FieldDemo> {
+  var _username = new TextEditingController();
+  var _password = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _username.text="初始值";
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("fieldDemo"),),
       body: Padding(
         padding: EdgeInsets.all(20),
+        // child: TextDemo(),
         child: Column(
           children: <Widget>[
-            TextField(),
-            SizedBox(height: 20,),
             TextField(
+              controller: _username,
               decoration: InputDecoration(
-                hintText: "请输入搜索的内容",
-                border: OutlineInputBorder(),
+                hintText: "请输入用户名"
               ),
+              onChanged: (value){
+               _username.text = value;
+
+              },
             ),
-            SizedBox(height: 20,),
-            TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                hintText: "多行文本框",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20,),
+            SizedBox(height: 10,),
             TextField(
               obscureText: true,
+              controller: _password,
               decoration: InputDecoration(
-                hintText: "密码框",
-                border: OutlineInputBorder(),
+                  hintText: "请输入密码"
+              ),
+              onChanged: (value){
+                _password.text = value;
+
+              },
+            ),
+
+            SizedBox(height: 20,),
+            Container(
+              width: double.infinity,
+              height: 40,
+              child: RaisedButton(
+                child: Text("登录"),
+                onPressed: (){
+                  print(this._username.text+"--"+this._password.text);
+                },
+                color: Colors.blue,
+                textColor: Colors.white,
               ),
             ),
-            SizedBox(height: 20,),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "用户名"
-              ),
-            ),
-            SizedBox(height: 20,),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "密码"
-              ),
-            ),
-            SizedBox(height: 20,),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                icon:Icon(Icons.person),
-                hintText:"请输入用户名"
-              ),
-            )
           ],
         ),
-
       ),
     );
   }
 }
+
+class TextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(),
+        SizedBox(height: 20,),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "请输入搜索的内容",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextField(
+          maxLines: 4,
+          decoration: InputDecoration(
+            hintText: "多行文本框",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "密码框",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "用户名"
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "密码"
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+              icon:Icon(Icons.person),
+              hintText:"请输入用户名"
+          ),
+        )
+      ],
+    );
+  }
+}
+
